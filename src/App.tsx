64a86df23cc17136c4712038bb2512e69b28c64f
warqa434-hue <warqa434@gmail.com>
2026-04-01 18:08:12 +0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
-          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/services" element={<HOWWEPROTECTYOUPage />} />
           <Route path="/services/home-builders" element={<HomeBuildersService />} />
           <Route path="/services/developers-smes" element={<DevelopersSMEsService />} />
           <Route path="/services/project-management" element={<ProjectManagementService />} />
