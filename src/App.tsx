3fa41d58226cd105ca50bd9e4bf27698c4492464
warqa434-hue <warqa434@gmail.com>
2026-04-01 18:03:24 +0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
-import ServicesPage from './pages/ServicesPage';
+import HOWWEPROTECTYOUPage from './pages/ServicesPage';
 import HomeBuildersService from './pages/services/HomeBuildersService';
 import DevelopersSMEsService from './pages/services/DevelopersSMEsService';
 import ProjectManagementService from './pages/services/ProjectManagementService';
